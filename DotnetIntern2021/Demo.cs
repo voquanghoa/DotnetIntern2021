c201e1e260406b21d3086c7dbbc0476afa074390
Vo Quang Hoa <voquanghoa@gmail.com>
2021-06-07 00:54:27 +0700

    Init project

---
+++ DotnetIntern2021/Demo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetIntern2021
+{
+    public class Demo
+    {
+        public int Sum(int a, int b) => a + b;
+
+        public int Substract(int a, int b) => a - b;
+
+        public int Max(int a, int b, int c)
+        {
+            if(a > b && a > c)
+            {
+                return a;
+            }
+
+            if(b > c)
+            {
+                return b;
+            }
+
+            return c;
+        }
+    }
+}
---
+++ DotnetIntern2021Test/DemoTest.cs
@@ -0,0 +1,35 @@
+﻿using DotnetIntern2021;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetIntern2021Test
+{
+    public class DemoTest
+    {
+        private readonly Demo demo = new();
+
+        [Test]
+        public void TestSum()
+        {
+            Assert.AreEqual(demo.Sum(2, 3), 5);
+        }
+
+        [TestCase(4, 2, 2)]
+        [TestCase(9, 3, 6)]
+        public void TestSubstract(int a, int b, int expected)
+        {
+            Assert.AreEqual(demo.Substract(a, b), expected);
+        }
+
+        [TestCase(4, 2, 2, 4)]
+        [TestCase(1, 3, 6, 6)]
+        public void TestMax(int a, int b, int c, int expected)
+        {
+            Assert.AreEqual(demo.Max(a, b, c), expected);
+        }
+    }
+}
