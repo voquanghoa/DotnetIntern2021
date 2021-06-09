6a529d4eed75be96e47662524054f8e38350d9e2
luuhieu8720 <55595475+luuhieu8720@users.noreply.github.com>
2021-06-09 15:48:52 +0700

    Add files via upload

---
+++ Exercise1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ExerciseBasic
+{
+    class Exercise1
+    {
+        public int UCLN(int a, int b)
+        {
+            while (a != b)
+            {
+                if (a > b) a = a - b;
+                else b = b - a;
+            }
+            return a;
+        }
+        public int BCNN(int a, int b)
+        {
+            int temp1 = a, temp2 = b;
+            while (temp1 != temp2)
+            {
+                if (temp1 > temp2) temp1 = temp1 - temp2;
+                else temp2 = temp2 - temp1;
+            }
+            return (a * b) / temp2;
+        }
+    }
+}
