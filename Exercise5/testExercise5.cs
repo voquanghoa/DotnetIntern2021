ffcbb70d1c09fabec782596a31ec6f0f2fc9e7e3
luuhieu8720 <55595475+luuhieu8720@users.noreply.github.com>
2021-06-10 11:58:26 +0700

    Unit test exercise5

---
+++ Exercise5/testExercise5.cs
@@ -0,0 +1,21 @@
+using DotnetIntern2021;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetIntern2021Test
+{
+    public class DemoTest
+    {
+        private readonly Exercise5 exercise5 = new();
+        [TestCase(160, new[] { 153 })]
+        [TestCase(5000, new[] { 153, 370, 371, 407, 1634 })]
+        public void TestExercise4(int a, int[] expected)
+        {
+            Assert.AreEqual(exercise5.bai5(a), expected);
+        }
+    }
+}
