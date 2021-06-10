14ce5ef3a10cefa3c490e2cb792a0625dcb9801d
luuhieu8720 <55595475+luuhieu8720@users.noreply.github.com>
2021-06-10 09:54:58 +0700

    Create Exercise3.cs

---
+++ Exercise-3/Exercise3.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetIntern2021
+{
+    public class Exercise3
+    {
+        public string prime(int n)
+        {
+            string answer = "";
+            int i = 2;
+            while (n > 1)
+            {
+                if (n % i == 0)
+                {
+                    answer = answer + "*" + i.ToString();
+                    n = n / i;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            answer = answer.Remove(0,1);
+            return answer;
+        }
+    }
+
+}
