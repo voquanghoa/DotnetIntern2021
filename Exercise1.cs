07567ca745c76633c00651b43a64021711146f8f
luuhieu8720 <55595475+luuhieu8720@users.noreply.github.com>
2021-06-10 08:33:34 +0700

    Update tái sử dụng UCLN cho hàm BCNN

--- Exercise1.cs
+++ Exercise1.cs
@@ -15,13 +15,7 @@ namespace ExerciseBasic
         }
         public int BCNN(int a, int b)
         {
-            int temp1 = a, temp2 = b;
-            while (temp1 != temp2)
-            {
-                if (temp1 > temp2) temp1 = temp1 - temp2;
-                else temp2 = temp2 - temp1;
-            }
-            return (a * b) / temp2;
+            return (a * b) / UCLN(a,b);
         }
     }
 }
