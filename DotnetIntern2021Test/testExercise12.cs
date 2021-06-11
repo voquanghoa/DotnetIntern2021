53a5c12139a1ac1a172bbd2514bf84d5997a42a4
luuhieu8720 <55595475+luuhieu8720@users.noreply.github.com>
2021-06-11 11:33:39 +0700

    Add files via upload

---
+++ DotnetIntern2021Test/testExercise12.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotnetIntern2021;
+using NUnit.Framework;
+namespace DotnetIntern2021Test
+{
+    class testExercise12
+    {
+        private readonly Exercise12 exercise12 = new Exercise12();
+        [TestCase(121,121)]
+        [TestCase(333,333)]
+        [TestCase(2345,5432)]
+        public void test(int a, int expected)
+        {
+            Assert.AreEqual(exercise12.palindrome(a), expected);
+        }
+    }
+}
