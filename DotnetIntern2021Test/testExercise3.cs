22012dd9a3f983ac53acdb40b77c6b06be5982c1
luuhieu8720 <55595475+luuhieu8720@users.noreply.github.com>
2021-06-11 08:47:12 +0700

    Create testExercise3.cs

---
+++ DotnetIntern2021Test/testExercise3.cs
@@ -0,0 +1,23 @@
+using DotnetIntern2021;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetIntern2021Test
+{
+    public class testExercise3
+    {
+        private readonly Exercise3 exercise3 = new();
+
+        [TestCase(10,"2*5")]
+        [TestCase(9, "3*3")]
+        [TestCase(600, "2*2*2*3*5*5")]
+        public void TestExercise3(int a, string expected)
+        {
+            Assert.AreEqual(exercise3.prime(a), expected);
+        }
+    }
+}
