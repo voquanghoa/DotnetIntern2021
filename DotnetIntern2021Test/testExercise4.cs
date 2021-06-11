319ad093e6b3fb280eb120d1b933bcb5fad04b53
luuhieu8720 <55595475+luuhieu8720@users.noreply.github.com>
2021-06-11 08:48:17 +0700

    Create testExercise4.cs

---
+++ DotnetIntern2021Test/testExercise4.cs
@@ -0,0 +1,23 @@
+using DotnetIntern2021;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetIntern2021Test
+{
+    public class DemoTest
+    {
+        private readonly Exercise4 exercise4 = new();
+
+        [TestCase(6, new[] { 1, 2, 3, 5 })]
+        [TestCase(1, new[] { 1 })]
+        [TestCase(13, new[] { 1, 2, 3, 5, 8 })]
+        public void TestExercise4(int a, int[] expected)
+        {
+            Assert.AreEqual(exercise4.fibo(a),expected);
+        }
+    }
+}
