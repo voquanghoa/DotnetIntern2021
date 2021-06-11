27fef001a8ebe06f75281110fa545b5305707a80
luuhieu8720 <55595475+luuhieu8720@users.noreply.github.com>
2021-06-11 11:18:53 +0700

    Add files via upload

---
+++ DotnetIntern2021Test/testExercise11.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotnetIntern2021;
+using NUnit.Framework;
+namespace DotnetIntern2021Test
+{
+    class testExercise11
+    {
+        private readonly Exercise11 exercise11 = new Exercise11();
+        [TestCase(10,5)]
+        [TestCase(100, 19)]
+        public void test(int a, int expected)
+        {
+            Assert.AreEqual(exercise11.reverse(a), expected);
+        }
+    }
+}
