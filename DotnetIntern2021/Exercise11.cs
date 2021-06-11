a484f0f742de53dd43dfa3aeb291dd1d98f35c74
luuhieu8720 <55595475+luuhieu8720@users.noreply.github.com>
2021-06-11 11:18:36 +0700

    Add files via upload

---
+++ DotnetIntern2021/Exercise11.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetIntern2021
+{
+    public class Exercise11
+    {
+        public int reverse(int n)
+        {
+            String s = radixChange(n);
+            int m = 0;
+            int h = s.Length - 1;
+            for (int i = 0; i <= h; i++)
+            {
+                if (s[i].CompareTo('1') == 0)
+                {
+                    m = m + Convert.ToInt32(Math.Pow(2, h - i));
+                }
+            }
+            return m;
+        }
+        public string radixChange(int n)
+        {
+            String a = "";
+            for (int i = 0; n > 0; i++)
+            {
+                a = a + (n % 2).ToString();
+                n = n / 2;
+            }
+            int h = a.Length;
+            return a;
+        }
+    }
+}
