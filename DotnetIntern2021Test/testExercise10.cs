6e63a364f924b50e19bb555a3ad6f388ed3efeb1
luuhieu8720 <55595475+luuhieu8720@users.noreply.github.com>
2021-06-11 10:12:04 +0700

    Add files via upload

---
+++ DotnetIntern2021Test/testExercise10.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotnetIntern2021;
+using NUnit.Framework;
+namespace DotnetIntern2021Test
+{
+    class testExercise10
+    {
+        private readonly Exercise10 exercise10 = new Exercise10();
+        [TestCase(8,2)]
+        [TestCase(9,2.0801)]
+        [TestCase(-25, -2.924)]
+        public void test(double a, double expected)
+        {
+            Assert.AreEqual(exercise10.cubeRoot(a), expected);
+        }
+    }
+}
