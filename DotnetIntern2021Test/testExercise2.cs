bd98ba8bea08af88ba2e5602df897e2c62007c68
luuhieu8720 <55595475+luuhieu8720@users.noreply.github.com>
2021-06-11 08:43:04 +0700

    Create testExercise2.cs

---
+++ DotnetIntern2021Test/testExercise2.cs
@@ -0,0 +1,23 @@
+using DotnetIntern2021;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetIntern2021Test
+{
+    public class testExercise2
+    {
+        private readonly Exercise2 exercise2 = new();
+
+        [TestCase(153,9)]
+        [TestCase(6, 6)]
+        [TestCase(10, 1)]
+        public void TestExercise2(int a, int expected)
+        {
+            Assert.AreEqual(exercise2.sumCS(a), expected);
+        }
+    }
+}
