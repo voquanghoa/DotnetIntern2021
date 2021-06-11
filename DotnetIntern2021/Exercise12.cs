e68fcaed58c43bd793c90d1d1cc6c4b9badfd1a7
luuhieu8720 <55595475+luuhieu8720@users.noreply.github.com>
2021-06-11 11:33:25 +0700

    Add files via upload

---
+++ DotnetIntern2021/Exercise12.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetIntern2021
+{
+    public class Exercise12
+    {
+        public int palindrome(int n)
+        {
+            string s = n.ToString();
+            String temp = "";
+            for (int i = s.Length - 1; i >=0; i--)
+            {
+                temp += s[i];
+            }
+            return Convert.ToInt32(temp);
+        }
+    }
+}
