aa4a4674ce05e3e36f045a49834a5b9de71938fc
luuhieu8720 <55595475+luuhieu8720@users.noreply.github.com>
2021-06-11 09:21:48 +0700

    Add files via upload

---
+++ DotnetIntern2021/Exercise7.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Exercise7
+{
+	public int giaiThua(int n)
+	{
+        if (n == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return n * giaiThua(n - 1);
+        }
+    }
+}
