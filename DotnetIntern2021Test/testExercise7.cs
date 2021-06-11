ffcde6e5ab68d2f8fb4ba2aa62b5b35647c0c8c3
luuhieu8720 <55595475+luuhieu8720@users.noreply.github.com>
2021-06-11 09:22:08 +0700

    Add files via upload

---
+++ DotnetIntern2021Test/testExercise7.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotnetIntern2021;
+using NUnit.Framework;
+
+namespace DotnetIntern2021Test
+{
+    class testExercise7
+    {
+        private readonly Exercise7 exercise7 = new();
+        [TestCase(5, 120)]
+        [TestCase(9, 362880)]
+        [TestCase(1, 1)]
+        public void TestExercise2(int a, int expected)
+        {
+            Assert.AreEqual(exercise7.giaiThua(a), expected);
+        }
+    }
+}
