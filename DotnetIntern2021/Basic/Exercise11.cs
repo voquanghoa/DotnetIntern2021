09be09ebb64ca4ceaada3c24fe5f408de1086050
luuhieu8720 <55595475+luuhieu8720@users.noreply.github.com>
2021-06-12 16:48:13 +0700

    Update and rename DotnetIntern2021/Exercise11.cs to DotnetIntern2021/Basic/Exercise11.cs

--- DotnetIntern2021/Basic/Exercise11.cs
+++ DotnetIntern2021/Basic/Exercise11.cs
@@ -1,10 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DotnetIntern2021
+namespace DotnetIntern2021.Basic
 {
     public class Exercise11
     {
