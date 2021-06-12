83d088a68076e01f47e777b1a770403b3984c137
luuhieu8720 <55595475+luuhieu8720@users.noreply.github.com>
2021-06-12 16:39:03 +0700

    Update and rename DotnetIntern2021/Exercise6.cs to DotnetIntern2021/Basic/Exercise6.cs

---
+++ DotnetIntern2021/Basic/Exercise6.cs
@@ -0,0 +1,13 @@
+using System;
+namespace DotnetIntern2021.Basic
+{
+	public class Exercise6
+	{
+		public int sum(int n)
+		{
+			int length = n * (n + 1);
+			int ans = (length + 1) * length / 2;
+			return ans * 2 - 1 - length;
+		}
+	}
+}
