8cfec8edd5aae0fbc78a04d3652d90e73f2a5ec5
luuhieu8720 <55595475+luuhieu8720@users.noreply.github.com>
2021-06-12 16:44:09 +0700

    Update testExercise8.cs

--- DotnetIntern2021Test/testExercise8.cs
+++ DotnetIntern2021Test/testExercise8.cs
@@ -1,6 +1,7 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using DotnetIntern2021.Basic;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
