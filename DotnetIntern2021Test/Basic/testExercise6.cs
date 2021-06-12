d2f8122d8c4267daa8131f27605b69a7a7b11299
luuhieu8720 <55595475+luuhieu8720@users.noreply.github.com>
2021-06-12 16:50:35 +0700

    Update testExercise6.cs

--- DotnetIntern2021Test/Basic/testExercise6.cs
+++ DotnetIntern2021Test/Basic/testExercise6.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using DotnetIntern2021.Basic;
 using NUnit.Framework;
 
-namespace DotnetIntern2021Test
+namespace DotnetIntern2021Test.Basic
 {
     class testExercise6
     {
