f01574bb2773284f38775f678ba2e2a05e28a611
luuhieu8720 <55595475+luuhieu8720@users.noreply.github.com>
2021-06-12 16:43:23 +0700

    Update and rename DotnetIntern2021/Exercise8.cs to DotnetIntern2021/Basic/Exercise8.cs

---
+++ DotnetIntern2021/Basic/Exercise8.cs
@@ -0,0 +1,20 @@
+using System;
+namespace DotnetIntern2021.Basic{
+	public class Exercise8
+	{
+		public double pi()
+		{
+			int i = 3;
+			double s = 0;
+			int check = 1;
+			while (i <= 5000)
+			{
+				s = s + Math.Pow(-1, check) * (Math.Pow(i, -1));
+				if (check == 1) check = 2;
+				else check = 1;
+				i += 2;
+			}
+			return Math.Round((1 + s) * 4,4);
+		}
+	}
+}
