3e6ed432759af35ec96a5feda8ddd5b1a961bac6
luuhieu8720 <55595475+luuhieu8720@users.noreply.github.com>
2021-06-12 16:39:36 +0700

    Update testExercise6.cs

--- DotnetIntern2021Test/testExercise6.cs
+++ DotnetIntern2021Test/testExercise6.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using DotnetIntern2021;
+using DotnetIntern2021.Basic;
 using NUnit.Framework;
 
 namespace DotnetIntern2021Test
