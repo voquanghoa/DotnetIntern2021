9c257786e5df2bc7e9ef2fe5379c4888ce953f99
luuhieu8720 <55595475+luuhieu8720@users.noreply.github.com>
2021-06-13 09:30:27 +0700

    Exercise 15

---
+++ DotnetIntern2021/Basic/Exercise15.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetIntern2021.Basic
+{
+    public class Exercise15
+    {
+        public int findDigits(int n)
+        {
+            if (n < 0) return 0;
+
+            if (n <= 1) return 1;
+
+            double digits = 0;
+            for (int i = 2; i <= n; i++)
+            {
+                digits += Math.Log10(i);
+            }
+
+            return (int)Math.Floor(digits) + 1;
+        }
+    }
+}
