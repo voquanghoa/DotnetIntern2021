7d9ccd559ad55303b33b7eee0bfcf39935d50e04
luuhieu8720 <55595475+luuhieu8720@users.noreply.github.com>
2021-06-14 14:34:45 +0700

    Update and rename testExercise5.cs to TestExercise5.cs

--- DotnetIntern2021Test/Basic/TestExercise5.cs
+++ DotnetIntern2021Test/Basic/TestExercise5.cs
@@ -8,14 +8,14 @@ using System.Threading.Tasks;
 
 namespace DotnetIntern2021Test.Basic
 {
-    public class testExercise5
+    public class TestExercise5
     {
         private readonly Exercise5 exercise5 = new();
         [TestCase(160, new[] { 153 })]
         [TestCase(5000, new[] { 153, 370, 371, 407, 1634 })]
-        public void TestExercise4(int a, int[] expected)
+        public void TestEx5(int a, int[] expected)
         {
-            Assert.AreEqual(exercise5.bai5(a), expected);
+            Assert.AreEqual(exercise5.Bai5(a), expected);
         }
     }
 }
