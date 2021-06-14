98f1291a8d84a42a2113089d2aa2ab8bfe1de417
luuhieu8720 <55595475+luuhieu8720@users.noreply.github.com>
2021-06-14 14:40:46 +0700

    Update Exercise7.cs

--- DotnetIntern2021/Basic/Exercise7.cs
+++ DotnetIntern2021/Basic/Exercise7.cs
@@ -3,7 +3,7 @@ namespace DotnetIntern2021.Basic
 {
 	public class Exercise7
 	{
-		public int giaiThua(int n)
+		public int GiaiThua(int n)
 		{
 		if (n == 0)
 		{
@@ -11,7 +11,7 @@ namespace DotnetIntern2021.Basic
 		}
 		else
 		{
-		    return n * giaiThua(n - 1);
+		    return n * GiaiThua(n - 1);
 		}
 	    }
 	}
