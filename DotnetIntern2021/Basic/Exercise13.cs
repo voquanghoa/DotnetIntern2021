213a9358a9e2ede8204a4fcc7fc68362b584f78c
luuhieu8720 <55595475+luuhieu8720@users.noreply.github.com>
2021-06-14 14:08:39 +0700

    Update Exercise13.cs

--- DotnetIntern2021/Basic/Exercise13.cs
+++ DotnetIntern2021/Basic/Exercise13.cs
@@ -1,11 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 namespace DotnetIntern2021.Basic
 {
     public class Exercise13
     {
-        public List<String> RestoreIpAddresses(String s)
-
+        public List<String> restoreIpAddresses(String s)
         {
             var result = new List<String>();
             for (int a = 0; a <= 255; a++)
@@ -15,33 +14,23 @@ namespace DotnetIntern2021.Basic
                     for (int b = 0; b <= 255; b++)
                     {
                         var ip2 = ip1 + b;
-
                         if (s.StartsWith(ip2))
-                        {
-                        ....
-                        }
-
                             for (int c = 0; c <= 255; c++)
                             {
                                 var ip3 = ip2 + c;
-
                                 if (s.StartsWith(ip3))
                                     for (int d = 0; d <= 255; d++)
                                     {
-                                        if (string.Equals(ip3, d))
-
+                                        if (string.Equals(s, ip3 + d))
                                         {
                                             result.Add($"{a}.{b}.{c}.{d}");
-
+                                            Console.WriteLine($"{a}.{b}.{c}.{d}");
                                         }
                                     }
                             }
                     }
             }
-
             return result;
         }
-
-
     }
 }
