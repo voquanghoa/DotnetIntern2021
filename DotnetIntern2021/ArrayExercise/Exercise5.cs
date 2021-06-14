71fba3d1646f9448336b5c13c119babcefffb7a4
luuhieu8720 <lethiluuhieu@gmail.com>
2021-06-14 16:35:11 +0700

    Add Array-05

---
+++ DotnetIntern2021/ArrayExercise/Exercise5.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetIntern2021.ArrayExercise
+{
+    public class Exercise5
+    {
+        public List<int> SubArray(int[] arr)
+        {
+            int n = arr.Length;
+            int max = 0, len = 0;
+            var answer = new List<int>();
+            var temp = new List<int>();
+            for (int i = 1; i < n; i++)
+            {
+                if (arr[i] > arr[i - 1])
+                {
+                    if (len == 0)
+                    {
+                        temp.Add(arr[i - 1]);
+                        len++;
+                    }
+                    temp.Add(arr[i]);
+                    len++;
+                }
+                else
+                {
+                    if (max < len)
+                    {
+                        max = len;
+                        answer.Clear();
+                        answer = temp.ToList();
+                    }
+                    temp.Clear();
+                    len = 0;
+                }
+            }
+            return answer;
+        }
+    }
+}
