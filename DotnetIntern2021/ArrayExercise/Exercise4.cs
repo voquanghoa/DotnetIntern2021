dfb74972503ca5d29fcfdc609ac2d36b31afc5ab
luuhieu8720 <lethiluuhieu@gmail.com>
2021-06-14 15:30:07 +0700

    Added exercise 04

---
+++ DotnetIntern2021/ArrayExercise/Exercise4.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetIntern2021.ArrayExercise
+{
+    public class Exercise4
+    {
+        static int MAX = 1000000;
+        int[] checkPrime = new int[MAX + 2];
+        public int Ex4(int[] a)
+        {
+            int sum = 0;
+            Eratosthenes();
+            int n = a.Length;
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(a[i].ToString() + ": " +  checkPrime[a[i]].ToString());
+                if (checkPrime[a[i]] == 1) sum += a[i];
+            }
+            return sum;
+        }
+        public void Eratosthenes()
+        {
+            for (int i = 2; i <= MAX; i++)
+            {
+                checkPrime[i] = 1;
+            }
+            for (int i = 2; i <= MAX; i++)
+            {
+                if (checkPrime[i] == 1)
+                {
+                    for (int j = i * 2; j <= MAX; j += i)
+                    {
+                        checkPrime[j] = 0;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ DotnetIntern2021Test/ArrayExercise/TestExercise4.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotnetIntern2021.ArrayExercise;
+using NUnit.Framework;
+namespace DotnetIntern2021Test.ArrayExercise
+{
+    class TestExercise4
+    {
+        private readonly Exercise4 exercise4 = new();
+        [TestCase(new[] { 1, 2, 3, 4, 5, 6, 11, 15, 17 },38)]
+        [TestCase(new[] { 4, 6, 8 }, 0)]
+        [TestCase(new[] { 0, 1, 3, 8 }, 3)]
+        public void TestEx4(int[] a, int expected)
+        {
+            Assert.AreEqual(exercise4.Ex4(a), expected);
+        }
+    }
+}
