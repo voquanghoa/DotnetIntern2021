e593b03c128233801aa58f9ca04dd5de7a33919d
luuhieu8720 <55595475+luuhieu8720@users.noreply.github.com>
2021-06-14 14:27:48 +0700

    Update TestExercise3.cs

--- DotnetIntern2021Test/Basic/TestExercise3.cs
+++ DotnetIntern2021Test/Basic/TestExercise3.cs
@@ -15,7 +15,7 @@ namespace DotnetIntern2021Test.Basic
         [TestCase(10,"2*5")]
         [TestCase(9, "3*3")]
         [TestCase(600, "2*2*2*3*5*5")]
-        public void TestExercise3(int a, string expected)
+        public void TestEx3(int a, string expected)
         {
             Assert.AreEqual(exercise3.Prime(a), expected);
         }
