51b8b020d203c4a2647f8a8b35c2617770aa39e0
luuhieu8720 <55595475+luuhieu8720@users.noreply.github.com>
2021-06-14 15:14:58 +0700

    Exercise18

---
+++ DotnetIntern2021/Basic/Exercise18.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetIntern2021.Basic
+{
+    public class Exercise18
+    {
+        public bool HappyNumber(int n)
+        {
+            int temp = n;
+            while (true)
+            {
+                int sum = 0;
+                while (temp > 0)
+                {
+                    sum += Convert.ToInt32(Math.Pow(temp % 10, 2));
+                    temp /= 10;
+                }
+                if (sum < 10)
+                {
+                    if (sum == 1 || sum == 7) return true;
+                    else return false;
+                }
+                temp = sum;
+            }
+        }
+    }
+}
