86afb405dfe18e92c5e0fd91da843bfca67eb9c4
luuhieu8720 <lethiluuhieu@gmail.com>
2021-06-14 17:25:33 +0700

    Add array-06

---
+++ DotnetIntern2021Test/ArrayExercise/TestExercise6.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using DotnetIntern2021.ArrayExercise;
+namespace DotnetIntern2021Test.ArrayExercise
+{
+    class TestExercise6
+    {
+        private readonly Exercise6 exercise6 = new();
+        [TestCase(new[] { 1, 2, 6, 4, 10 }, 3, -1)]
+        [TestCase(new[] { 1, 3, 5, 7, 9 }, 3, 1)]
+        public void Test(int[] a, int x, int expected)
+        {
+            Assert.AreEqual(exercise6.Search(a,x,0,5),expected);
+        }
+    }
+}
