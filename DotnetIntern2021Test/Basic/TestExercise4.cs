45b2b9f58a6edd1019e26183ff1134e3f72bb02e
luuhieu8720 <55595475+luuhieu8720@users.noreply.github.com>
2021-06-14 14:29:24 +0700

    Update and rename testExercise4.cs to TestExercise4.cs

--- DotnetIntern2021Test/Basic/TestExercise4.cs
+++ DotnetIntern2021Test/Basic/TestExercise4.cs
@@ -8,14 +8,14 @@ using System.Threading.Tasks;
 
 namespace DotnetIntern2021Test.Basic
 {
-    public class Test
+    public class TestExercise4
     {
         private readonly Exercise4 exercise4 = new();
 
         [TestCase(6, new[] { 1, 2, 3, 5 })]
         [TestCase(1, new[] { 1 })]
         [TestCase(13, new[] { 1, 2, 3, 5, 8 })]
-        public void TestExercise4(int a, int[] expected)
+        public void TestEx4(int a, int[] expected)
         {
             Assert.AreEqual(exercise4.Fibo(a), expected);
         }
