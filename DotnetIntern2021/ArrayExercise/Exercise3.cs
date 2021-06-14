6ee528a33b16ff4eb10dbb6bba2c9725b64cfd8a
luuhieu8720 <lethiluuhieu@gmail.com>
2021-06-14 15:28:09 +0700

    Add exercise 03

---
+++ DotnetIntern2021/ArrayExercise/Exercise3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetIntern2021.ArrayExercise
+{
+    public class Exercise3
+    {
+        public int Ex3(int[] a)
+        {
+            int n = a.Length;
+            int odd = 0, even = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (a[i] % 2 == 0) even += a[i];
+                else odd += a[i];
+            }
+            return even - odd;
+        }
+    }
+}
---
+++ DotnetIntern2021Test/ArrayExercise/TestExercise3.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotnetIntern2021.ArrayExercise;
+using NUnit.Framework;
+namespace DotnetIntern2021Test.ArrayExercise
+{
+    class TestExercise3
+    {
+        private readonly Exercise3 exercise3 = new();
+        [TestCase(new[] { 1, 2, 3, 4, 5, 6 },3)]
+        [TestCase(new[] { 10, 12, 14 }, 36)]
+        [TestCase(new[] { 3, 5, 7 }, -15)]
+        public void TestEx3(int[] a, int expected)
+        {
+            Assert.AreEqual(exercise3.Ex3(a), expected);
+        }
+    }
+}
