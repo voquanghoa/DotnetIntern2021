63cd23a2b6888c23b21f860c35cd6d43977c30aa
luuhieu8720 <55595475+luuhieu8720@users.noreply.github.com>
2021-06-14 14:49:37 +0700

    Update and rename testExercise11.cs to TestExercise11.cs

--- DotnetIntern2021Test/Basic/TestExercise11.cs
+++ DotnetIntern2021Test/Basic/TestExercise11.cs
@@ -7,14 +7,14 @@ using DotnetIntern2021.Basic;
 using NUnit.Framework;
 namespace DotnetIntern2021Test.Basic
 {
-    class testExercise11
+    class TestExercise11
     {
         private readonly Exercise11 exercise11 = new Exercise11();
         [TestCase(10,5)]
         [TestCase(100, 19)]
-        public void test(int a, int expected)
+        public void Test(int a, int expected)
         {
-            Assert.AreEqual(exercise11.reverse(a), expected);
+            Assert.AreEqual(exercise11.Reverse(a), expected);
         }
     }
 }
