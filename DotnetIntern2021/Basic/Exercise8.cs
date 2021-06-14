b8f1a94ddbed47cd14d0a0143292ee0212a2f4c0
luuhieu8720 <55595475+luuhieu8720@users.noreply.github.com>
2021-06-14 14:43:24 +0700

    Update Exercise8.cs

--- DotnetIntern2021/Basic/Exercise8.cs
+++ DotnetIntern2021/Basic/Exercise8.cs
@@ -1,20 +1,22 @@
 using System;
-namespace DotnetIntern2021.Basic{
-	public class Exercise8
-	{
-		public double pi()
-		{
-			int i = 3;
-			double s = 0;
-			int check = 1;
-			while (i <= 5000)
-			{
-				s = s + Math.Pow(-1, check) * (Math.Pow(i, -1));
-				if (check == 1) check = 2;
-				else check = 1;
-				i += 2;
-			}
-			return Math.Round((1 + s) * 4,4);
-		}
-	}
+namespace DotnetIntern2021.Basic
+{
+    public class Exercise8
+    {
+        public double Pi()
+        {
+            int i = 3;
+            double s = 0;
+            int check = 1;
+            while (i <= 5000)
+            {
+                s = s + Math.Pow(-1, check) * (Math.Pow(i, -1));
+                if (check == 1) check = 2;
+                else check = 1;
+                i += 2;
+            }
+            return Math.Round((1 + s) * 4, 4);
+        }
+    }
+
 }
