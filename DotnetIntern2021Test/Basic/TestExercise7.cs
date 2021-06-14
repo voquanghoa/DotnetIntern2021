e87de1024db762617389cc06bdda7afcea54e043
luuhieu8720 <55595475+luuhieu8720@users.noreply.github.com>
2021-06-14 14:40:49 +0700

    Update and rename testExercise7.cs to TestExercise7.cs

--- DotnetIntern2021Test/Basic/TestExercise7.cs
+++ DotnetIntern2021Test/Basic/TestExercise7.cs
@@ -8,15 +8,15 @@ using NUnit.Framework;
 
 namespace DotnetIntern2021Test.Basic
 {
-    class testExercise7
+    class TestExercise7
     {
         private readonly Exercise7 exercise7 = new();
         [TestCase(5, 120)]
         [TestCase(9, 362880)]
         [TestCase(1, 1)]
-        public void TestExercise2(int a, int expected)
+        public void TestEx7(int a, int expected)
         {
-            Assert.AreEqual(exercise7.giaiThua(a), expected);
+            Assert.AreEqual(exercise7.GiaiThua(a), expected);
         }
     }
 }
