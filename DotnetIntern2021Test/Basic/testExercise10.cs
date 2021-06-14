829cda0ce9b5b1a2268a1f658a826479c9243d3c
luuhieu8720 <55595475+luuhieu8720@users.noreply.github.com>
2021-06-14 14:47:13 +0700

    Update testExercise10.cs

--- DotnetIntern2021Test/Basic/testExercise10.cs
+++ DotnetIntern2021Test/Basic/testExercise10.cs
@@ -3,19 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using DotnetIntern2021.Basic;
+using DotnetIntern2021;
 using NUnit.Framework;
+using DotnetIntern2021.Basic;
 namespace DotnetIntern2021Test.Basic
 {
-    class testExercise10
+    public class TestExercise10
     {
-        private readonly Exercise10 exercise10 = new Exercise10();
+        private readonly Exercise10 exercise10 = new();
         [TestCase(8,2)]
         [TestCase(9,2.0801)]
         [TestCase(-25, -2.924)]
-        public void test(double a, double expected)
+        public void Test(double a, double expected)
         {
-            Assert.AreEqual(exercise10.cubeRoot(a), expected);
+            Assert.AreEqual(exercise10.CubeRoot(a), expected);
         }
     }
 }
