d6d7b39029122495507b49954ac446aa6906ebaa
luuhieu8720 <55595475+luuhieu8720@users.noreply.github.com>
2021-06-15 08:43:50 +0700

    Update Exercise5.cs

--- DotnetIntern2021/ArrayExercise/Exercise5.cs
+++ DotnetIntern2021/ArrayExercise/Exercise5.cs
@@ -10,32 +10,27 @@ namespace DotnetIntern2021.ArrayExercise
     {
         public List<int> SubArray(int[] arr)
         {
-            int n = arr.Length;
-            int max = 0, len = 0;
+            int max = 0;
             var answer = new List<int>();
             var temp = new List<int>();
-            for (int i = 1; i < n; i++)
+            for (int i = 1; i < arr.Length; i++)
             {
                 if (arr[i] > arr[i - 1])
                 {
-                    if (len == 0)
+                    if (temp.Count == 0)
                     {
                         temp.Add(arr[i - 1]);
-                        len++;
                     }
                     temp.Add(arr[i]);
-                    len++;
                 }
                 else
                 {
-                    if (max < len)
+                    if (max < temp.Count)
                     {
-                        max = len;
-                        answer.Clear();
+                        max = temp.Count;
                         answer = temp.ToList();
                     }
                     temp.Clear();
-                    len = 0;
                 }
             }
             return answer;
