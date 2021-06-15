50a845c2901bee9485b4aaec21c27aebb48e213d
luuhieu8720 <lethiluuhieu@gmail.com>
2021-06-15 11:22:07 +0700

    Add array-10

---
+++ DotnetIntern2021/ArrayExercise/Exercise10.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetIntern2021.ArrayExercise
+{
+    public class Exercise10
+    {
+        public int Ex10(int[] a)
+        {
+            int sum = 0;
+            foreach(int item in a)
+            {
+                int root = Convert.ToInt32(Math.Sqrt(item));
+                if (root * root == item) sum += item;
+            }
+            return sum;
+        }
+    }
+}
