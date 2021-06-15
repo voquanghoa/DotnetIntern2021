5dca53b6fef27f7ba621abd25be27998f284f10a
luuhieu8720 <lethiluuhieu@gmail.com>
2021-06-15 11:44:52 +0700

    Add string-01

---
+++ DotnetIntern2021/Strings/Exercise1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DotnetIntern2021.Strings
+{
+    public class Exercise1
+    {
+        public string snakeCase(String upperCase)
+        {
+            string snakeCase = "";
+            for (int i = 0; i < upperCase.Length; i++)
+            {
+                if (Convert.ToInt32(upperCase[i]) >= 65 && Convert.ToInt32(upperCase[i]) <= 90 && i != 0)
+                    snakeCase += "_";
+                snakeCase += upperCase[i];
+            }
+            return snakeCase.ToLower();
+        }
+    }
+}
---
+++ DotnetIntern2021Test/Strings/TestExercise1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotnetIntern2021.Strings;
+using NUnit.Framework;
+namespace DotnetIntern2021Test.Strings
+{
+    class TestExercise1
+    {
+        private readonly Exercise1 exercise1 = new();
+        [TestCase("MyProgram", "my_program")]
+        [TestCase("helloWorld", "hello_world")]
+        public void Test(string upperCase, string snakeCase)
+        {
+            Assert.AreEqual(exercise1.snakeCase(upperCase), snakeCase);
+        }
+    }
+}
