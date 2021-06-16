8f7b3bad39c048c0391913c0f2633b4f34648222
luuhieu8720 <55595475+luuhieu8720@users.noreply.github.com>
2021-06-16 11:26:14 +0700

    Update Exercise9.cs

--- DotnetIntern2021/Basic/Exercise9.cs
+++ DotnetIntern2021/Basic/Exercise9.cs
@@ -4,13 +4,13 @@ namespace DotnetIntern2021.Basic
 {
     public class Exercise9
     {
-        public double SquareRoot(double n)
+        public double SquareRoot(double n, double epsilon)
         {
             if (n == 0) return 0;
             if (n < 0) return -1;
-            double x = n;
-            double root = x - (x * x - n) / (2 * x); ;
-            while (Math.Abs(root - x) >= 0.0001)
+            var x = n;
+            var root = x - (x * x - n) / (2 * x); ;
+            while (Math.Abs(root - x) >= epsilon)
             {
                 x = root;
                 root = x - (x * x - n) / (2 * x);
