56ea7af2b9f6ef6abe77576f3bb6d4d4e8b9843b
luuhieu8720 <55595475+luuhieu8720@users.noreply.github.com>
2021-06-16 11:20:44 +0700

    Update Exercise10.cs

--- DotnetIntern2021/Basic/Exercise10.cs
+++ DotnetIntern2021/Basic/Exercise10.cs
@@ -4,9 +4,8 @@ namespace DotnetIntern2021.Basic
 {
     public class Exercise10
     {
-        public double CubeRoot(double n)
+        public double CubeRoot(double n, double epsilon)
         {
-            const double epsilon = 0.000001;
             var x = n;
             var root = x - (x * x * x - n) / (3 * x * x);
 
