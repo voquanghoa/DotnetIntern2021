b6877524021a69267bb20f08db2328ae27a99b90
luuhieu8720 <55595475+luuhieu8720@users.noreply.github.com>
2021-06-16 11:04:08 +0700

    Update DotnetIntern2021/Basic/Exercise15.cs
    Co-authored-by: Võ Quang Hòa <voquanghoa@gmail.com>

--- DotnetIntern2021/Basic/Exercise15.cs
+++ DotnetIntern2021/Basic/Exercise15.cs
@@ -10,9 +10,7 @@ namespace DotnetIntern2021.Basic
     {
         public int findDigits(int n)
         {
-            if (n < 0) return 0;
-
-            if (n <= 1) return 1;
+            if (n <= 1) return n;
 
             double digits = 0;
             for (int i = 2; i <= n; i++)
