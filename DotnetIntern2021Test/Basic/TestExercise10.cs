57dfcef49c4c2bff87028ed796412732c79b6fd0
luuhieu8720 <55595475+luuhieu8720@users.noreply.github.com>
2021-06-16 11:29:08 +0700

    Update TestExercise10.cs

--- DotnetIntern2021Test/Basic/TestExercise10.cs
+++ DotnetIntern2021Test/Basic/TestExercise10.cs
@@ -10,13 +10,14 @@ namespace DotnetIntern2021Test.Basic
 {
     public class TestExercise10
     {
+        const double epsilon = 0.000001;
         private readonly Exercise10 exercise10 = new();
-        [TestCase(8, 2, 0.000001)]
-        [TestCase(9, 2.0800838230519045, 0.000001)]
-        [TestCase(-25, -2.924017738212866, 0.000001)]
-        public void Test(double a, double expected, double epsilon)
+        [TestCase(8, 2)]
+        [TestCase(9, 2.0800838230519045)]
+        [TestCase(-25, -2.924017738212866)]
+        public void Test(double a, double expected)
         {
-            Assert.AreEqual(exercise10.CubeRoot(a,epsilon), expected);
+            Assert.AreEqual(exercise10.CubeRoot(a, epsilon), expected);
         }
     }
 }
