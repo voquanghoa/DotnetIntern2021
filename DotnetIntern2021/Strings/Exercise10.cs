006a2e76878c66337c3e0113844cd631c42bd122
luuhieu8720 <lethiluuhieu@gmail.com>
2021-06-17 09:42:35 +0700

    Add string-10

---
+++ DotnetIntern2021/Strings/Exercise10.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetIntern2021.Strings
+{
+    public class Exercise10
+    {
+        public List<string> SeparateFullName(string fullName)
+        {
+            int i = fullName.Length - 1;
+            while (fullName[i] != ' ')
+            {
+                i--;
+            }
+            var answer = new List<string>();
+            answer.Add(fullName.Substring(0, i));
+            answer.Add(fullName.Substring(i + 1, fullName.Length - i - 1));
+            return answer;
+        }
+
+    }
+}
---
+++ DotnetIntern2021Test/Strings/TestExercise10.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotnetIntern2021.Strings;
+using NUnit.Framework;
+namespace DotnetIntern2021Test.Strings
+{
+    class TestExercise10
+    {
+        private readonly Exercise10 exercise10 = new();
+        [Test]
+        public void Test()
+        {
+            string fullName = "Johnson Baby For Kids";
+            var expected = new List<string>(){ "Johnson Baby For", "Kids" };
+            Assert.AreEqual(exercise10.SeparateFullName(fullName), expected);
+        }
+    }
+}
