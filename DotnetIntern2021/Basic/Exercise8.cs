6330a687fa4095faa89ac85b17fd7769ca8f4032
luuhieu8720 <55595475+luuhieu8720@users.noreply.github.com>
2021-06-17 11:14:30 +0700

    Update Exercise8.cs

--- DotnetIntern2021/Basic/Exercise8.cs
+++ DotnetIntern2021/Basic/Exercise8.cs
@@ -3,17 +3,22 @@ namespace DotnetIntern2021.Basic
 {
     public class Exercise8
     {
+        const double epsilon = 0.00001;
         public double Pi()
         {
             var i = 3.0;
             var s = 0.0;
             int check = 1;
-            while (4 / (2 * i + 1) >= 0.00001)
+            while (4 / (2 * i + 1) >= epsilon)
             {
                 if (check % 2 == 0)
+                {
                     s += Math.Pow(i, -1);
-                else 
+                }
+                else
+                {
                     s -= Math.Pow(i, -1);
+                }
                 check++;
                 i += 2;
             }
