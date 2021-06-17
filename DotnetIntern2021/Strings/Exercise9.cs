128a4a6541305e1cd0049316a308a1e651e76aae
luuhieu8720 <lethiluuhieu@gmail.com>
2021-06-17 09:41:21 +0700

    Add string-09

---
+++ DotnetIntern2021/Strings/Exercise9.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetIntern2021.Strings
+{
+    public class Exercise9
+    {
+        public bool BarcodeEAN(string s)
+        {
+            var sum = 0;
+            for (var i = 1; i <= s.Length; i++)
+            {
+                if (i % 2 == 0) sum += 3 * ((int)s[i - 1] - 48);
+                else sum += ((int)s[i - 1] - 48);
+            }
+            return sum % 10 == 0;
+        }
+    }
+}
