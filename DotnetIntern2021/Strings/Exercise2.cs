13db80e6c3db5e758bff21cb2aac8c26167bc314
luuhieu8720 <lethiluuhieu@gmail.com>
2021-06-17 09:27:12 +0700

    Add string-02

---
+++ DotnetIntern2021/Strings/Exercise2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetIntern2021.Strings
+{
+    public class Exercise2
+    {
+        public string Ex2(string snakeCase)
+        {
+            string upperCase = "";
+            upperCase += snakeCase[0].ToString().ToUpper();
+            int i = 1;
+            while(i < snakeCase.Length)
+            {
+                if (string.Equals(snakeCase[i].ToString(), "_"))
+                {
+                    upperCase += snakeCase[i + 1].ToString().ToUpper();
+                    i++;
+                }
+                else upperCase += snakeCase[i];
+                i++;
+            }
+
+            return upperCase;
+        }
+    }
+}
