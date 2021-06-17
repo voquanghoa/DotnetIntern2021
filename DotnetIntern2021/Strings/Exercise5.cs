f12b198ad5ce62b2c2c8e16d0a91565ee1a37105
luuhieu8720 <lethiluuhieu@gmail.com>
2021-06-17 09:32:33 +0700

    Add string-05

---
+++ DotnetIntern2021/Strings/Exercise5.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetIntern2021.Strings
+{
+    public class Exercise5
+    {
+        public bool Palindrome(string s)
+        {
+            string left = "";
+            string right = "";
+            for (int i = s.Length/2 - 1; i>= 0 ; i--)
+            {
+                left += s[i];
+                right += s[s.Length - i - 1];
+            }
+            return left == right;
+        }
+    }
+}
