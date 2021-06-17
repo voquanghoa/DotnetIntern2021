b9ba77993f31675fb2e537b03ca7c7e2d758d69f
luuhieu8720 <lethiluuhieu@gmail.com>
2021-06-17 09:29:20 +0700

    Add string-03

---
+++ DotnetIntern2021/Strings/Exercise3.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetIntern2021.Strings
+{
+    public class Exercise3
+    {
+        public  string Reverse (string s)
+        {
+            string ans = "";
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                ans += s[i];
+            }
+            return ans;
+        }
+    }
+}
---
+++ DotnetIntern2021Test/Strings/TestExercise3.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotnetIntern2021.Strings;
+using NUnit.Framework;
+namespace DotnetIntern2021Test.Strings
+{
+    class TestExercise3
+    {
+        private readonly Exercise3 exercise3 = new();
+        [TestCase("abcdef","fedcba")]
+        [TestCase("","")]
+        [TestCase("123xyz","zyx321")]
+        public void Test(string s, string ans)
+        {
+            Assert.AreEqual(exercise3.Reverse(s), ans);
+        }
+    }
+}
