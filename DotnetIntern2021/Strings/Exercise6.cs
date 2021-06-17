1344974f231313295ee03fe3cb056474cc0ba15b
luuhieu8720 <lethiluuhieu@gmail.com>
2021-06-17 09:34:24 +0700

    Add string-06

---
+++ DotnetIntern2021/Strings/Exercise6.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetIntern2021.Strings
+{
+    public class Exercise6
+    {
+        public string Ex6(string s)
+        {
+            string ans = "";
+            int i = 0;
+            while (i < s.Length)
+            {
+                int count = 1;
+                while (i < s.Length - 1 && s[i] == s[i + 1])
+                {
+                    i++;
+                    count++;
+                }
+                if (count > 1)
+                {
+                    ans += s[i] + count.ToString();
+                }
+                else ans += s[i];
+                i++;
+            }
+            Console.WriteLine(ans);
+            return ans;
+        }
+    }
+}
