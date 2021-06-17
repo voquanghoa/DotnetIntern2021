ab66a7557902f7138207a977232acae485b3d880
luuhieu8720 <lethiluuhieu@gmail.com>
2021-06-17 09:31:06 +0700

    Add string-04

---
+++ DotnetIntern2021/Strings/Exercise4.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace DotnetIntern2021.Strings
+{
+    public class Exercise4
+    {
+        public int SumEx4(string s)
+        {
+            int sum = 0;
+            string tempString = "";
+            int i = 0;
+            while (i < s.Length)
+            {
+                if (s[i] >= '1' && s[i] <= '9')
+                {
+                    tempString += s[i];
+                }
+                else
+                {
+                    if (tempString != "") sum += Convert.ToInt32(tempString);
+                    tempString = "";
+                }
+                i++;
+            }
+            if (!string.Equals(tempString, "")) sum += Convert.ToInt32(tempString);
+            return sum;
+        }
+    }
+}
