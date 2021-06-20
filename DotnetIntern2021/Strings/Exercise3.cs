b1921dc3dae0101f3a9044a5e28cffa9f6dac454
luuhieu8720 <55595475+luuhieu8720@users.noreply.github.com>
2021-06-20 21:15:32 +0700

    Update Exercise3.cs

--- DotnetIntern2021/Strings/Exercise3.cs
+++ DotnetIntern2021/Strings/Exercise3.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,14 +8,14 @@ namespace DotnetIntern2021.Strings
 {
     public class Exercise3
     {
-        public  string Reverse (string s)
+        public string Reverse(string s)
         {
-            string ans = "";
+            StringBuilder answer = new StringBuilder();
             for (int i = s.Length - 1; i >= 0; i--)
             {
-                ans += s[i];
+                answer.Append(s[i]);
             }
-            return ans;
+            return answer.ToString();
         }
     }
 }
