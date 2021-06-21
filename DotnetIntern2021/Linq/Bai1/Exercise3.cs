99a3940661ee77c4867fd05965ff40d5e923fac9
luuhieu8720 <lethiluuhieu@gmail.com>
2021-06-21 09:28:09 +0700

    Add linq ex03

---
+++ DotnetIntern2021/Linq/Bai1/Exercise3.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetIntern2021.Linq.Bai1
+{
+    public class Exercise3
+    {
+        private readonly DataFactory dataFactory = new();
+        public List<string> FindStudentByClass(string className)
+        {
+            return dataFactory.Students
+                .Where(s => s.Class.Name
+                .Equals(className))
+                .Select(s => s.Name)
+                .ToList();
+        }
+    }
+}
