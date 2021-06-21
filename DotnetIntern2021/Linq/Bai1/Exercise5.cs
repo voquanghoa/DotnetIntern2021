cf9932a5fa22896cd3e60c291c779a6bc45eb65a
luuhieu8720 <lethiluuhieu@gmail.com>
2021-06-21 09:30:23 +0700

    Add linq ex05

---
+++ DotnetIntern2021/Linq/Bai1/Exercise5.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetIntern2021.Linq.Bai1
+{
+    public class Exercise5
+    {
+        private readonly DataFactory dataFactory = new();
+        public List<string> MaxScoreGroupByClass()
+        {
+            var result = new List<string>();
+            var groupedResult = dataFactory.Students.GroupBy(s => s.Class.Name);
+            foreach (var ageGroup in groupedResult)
+            {
+                var x = ageGroup.OrderByDescending(s => s.Score).First().Name;
+                result.Add(x);
+            }
+            return result;
+        }
+    }
+}
---
+++ DotnetIntern2021Test/Linq/Bai1/TestExercise5.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotnetIntern2021.Linq.Bai1;
+using NUnit.Framework;
+
+namespace DotnetIntern2021Test.Linq.Bai1
+{
+    class TestExercise5
+    {
+        private readonly Exercise5 exercise5 = new();
+        [Test]
+        public void Test()
+        {
+            var answer = new List<string>() { "John", "Hyun Bin" };
+            Assert.AreEqual(exercise5.MaxScoreGroupByClass(), answer);
+        }
+    }
+}
