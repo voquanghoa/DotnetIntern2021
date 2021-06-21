ee39e758dd31d7873807725350e150d76a077573
luuhieu8720 <lethiluuhieu@gmail.com>
2021-06-21 10:16:26 +0700

    Add linq ex07

---
+++ DotnetIntern2021/Linq/Bai1/Exercise7.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetIntern2021.Linq.Bai1
+{
+    public class Exercise7
+    {
+        private readonly DataFactory dataFactory = new();
+        public List<double> FindAllScore()
+        {
+            return dataFactory.Students
+                .Select(s => s.Score)
+                .Distinct()
+                .ToList();
+        }
+    }
+}
---
+++ DotnetIntern2021Test/Linq/Bai1/TestExercise7.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using DotnetIntern2021.Linq.Bai1;
+namespace DotnetIntern2021Test.Linq.Bai1
+{
+    class TestExercise7
+    {
+        private readonly Exercise7 exercise7 = new();
+        [Test]
+        public void Test()
+        {
+            var answer = new List<double>
+            {
+                9.0, 9.5, 8.0, 10.0, 7.0, 6.0, 4.0
+            };
+            Assert.AreEqual(exercise7.FindAllScore(), answer);
+        }
+    }
+}
