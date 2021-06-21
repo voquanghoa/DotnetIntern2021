72810a9cd92b2da2cb4cc9b0710acf37a860b8ea
luuhieu8720 <lethiluuhieu@gmail.com>
2021-06-21 09:29:33 +0700

    Add linq ex04

---
+++ DotnetIntern2021/Linq/Bai1/Exercise4.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetIntern2021.Linq.Bai1
+{
+    public class Exercise4
+    {
+        private readonly DataFactory dataFactory = new();
+        public double AverageScoreByClassname(string className)
+        {
+            return dataFactory.Students
+                .Where(s => s.Class.Name.Equals(className))
+                .Average(s => s.Score);
+        }
+    }
+}
