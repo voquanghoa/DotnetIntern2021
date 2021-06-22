ae5137d01a3c909448b6332459720e692bf630c3
luuhieu8720 <lethiluuhieu@gmail.com>
2021-06-22 10:07:30 +0700

    Add linq ex11

---
+++ DotnetIntern2021Test/Linq/Bai1/TestExercise11.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using DotnetIntern2021.Linq.Bai1;
+namespace DotnetIntern2021Test.Linq.Bai1
+{
+    class TestExercise11
+    {
+        private readonly Exercise11 exercise11 = new();
+        [Test]
+        public void TestEx11()
+        {
+            Assert.AreEqual(exercise11.FindStudentContainsName(), "Class A");
+        }
+    }
+}
