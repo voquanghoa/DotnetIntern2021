14c43f12489b5cfadfee063486b5e93615996736
luuhieu8720 <55595475+luuhieu8720@users.noreply.github.com>
2021-06-22 10:25:33 +0700

    Update DataFactory.cs

--- DotnetIntern2021/Linq/Bai1/DataFactory.cs
+++ DotnetIntern2021/Linq/Bai1/DataFactory.cs
@@ -24,6 +24,7 @@ namespace DotnetIntern2021.Linq.Bai1
             new Student(new Class() { Id = 1, Name = "Class A" }, 7, "Min Ho", new DateTime(1990, 6, 11), 9.0),
             new Student(new Class() { Id = 2, Name = "Class B" }, 8, "Yu Chun", new DateTime(1990, 1, 8), 4.0),
             new Student(new Class() { Id = 2, Name = "Class B" }, 9, "Mickey", new DateTime(1996, 2, 10), 4.0),
+            new Student(new Class() { Id = 2, Name = "Class B" }, 10, "Hello World", new DateTime(2002, 12, 20), 7.0),
         };
     }
 }
