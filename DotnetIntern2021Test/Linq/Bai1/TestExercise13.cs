57475b98037050e50dfc42be26d4bce795cbabbd
luuhieu8720 <lethiluuhieu@gmail.com>
2021-06-23 08:44:29 +0700

    Add linq ex13

---
+++ DotnetIntern2021Test/Linq/Bai1/TestExercise13.cs
@@ -0,0 +1,15 @@
+﻿using DotnetIntern2021.Linq.Bai1;
+using NUnit.Framework;
+
+namespace DotnetIntern2021Test.Linq.Bai1
+{
+    public class TestExercise13
+    {
+        [Test]
+        public void TestEx13()
+        {
+            var exercise13 = new Exercise13();
+            Assert.AreEqual(exercise13.CheckStudent(2000, 8.0), true);
+        }
+    }
+}
