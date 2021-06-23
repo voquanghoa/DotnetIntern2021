213bdf061783bc884bc0460e9acc6d9b1d03f115
luuhieu8720 <lethiluuhieu@gmail.com>
2021-06-23 10:20:06 +0700

    Add linq part2 ex02

---
+++ DotnetIntern2021Test/Linq/Bai2/TestExercise2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotnetIntern2021.Linq.Bai2;
+using NUnit.Framework;
+namespace DotnetIntern2021Test.Linq.Bai2
+{
+    class TestExercise2
+    {
+        private readonly Exercise2 exercise2 = new();
+        [Test]
+        public void TestEx01()
+        {
+            var answer = new List<int>()
+            {
+                -3, -2, -1
+            };
+            Assert.AreEqual(exercise2.ThreeHighestIndexNumbers(), answer);
+        }
+    }
+}
