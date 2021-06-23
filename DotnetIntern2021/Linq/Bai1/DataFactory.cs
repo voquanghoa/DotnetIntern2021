521015262119fed21c70706ab4837a18be40abec
luuhieu8720 <lethiluuhieu@gmail.com>
2021-06-23 09:14:47 +0700

    update datafactory

--- DotnetIntern2021/Linq/Bai1/DataFactory.cs
+++ DotnetIntern2021/Linq/Bai1/DataFactory.cs
@@ -16,8 +16,8 @@ namespace DotnetIntern2021.Linq.Bai1
         };
         public readonly List<Student> Students = new List<Student>()
         {
-            new Student(new Class() { Id = 1, Name = "Class A" }, 1, "John", new DateTime(2000, 2, 1), 9.0),
-            new Student(new Class() { Id = 2, Name = "Class B" }, 2, "David", new DateTime(2000, 7, 8), 9.5),
+            new Student(new Class() { Id = 1, Name = "Class A" }, 1, "John Leo", new DateTime(2000, 2, 1), 9.0),
+            new Student(new Class() { Id = 2, Name = "Class B" }, 2, "David Kim", new DateTime(2000, 7, 8), 9.5),
             new Student(new Class() { Id = 1, Name = "Class A" }, 3, "Black Pink", new DateTime(1996, 12, 4), 8.0),
             new Student(new Class() { Id = 2, Name = "Class B" }, 4, "Hyun Bin", new DateTime(1980, 5, 15), 10.0),
             new Student(new Class() { Id = 2, Name = "Class B" }, 5, "Dong Wook", new DateTime(1980, 5, 15), 7.0),
