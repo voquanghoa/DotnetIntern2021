1c68daf2fb45018ce527bf06b5ee544e7e993491
luuhieu8720 <lethiluuhieu@gmail.com>
2021-06-23 15:58:38 +0700

    Add linq part2 ex09

---
+++ DotnetIntern2021/Linq/Bai2/Exercise9.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetIntern2021.Linq.Bai2
+{
+    public class Exercise9
+    {
+        public int SumEven(List<int> data)
+        {
+            return data.Where(s => s % 2 == 0)
+                       .Sum();
+        }
+    }
+}
