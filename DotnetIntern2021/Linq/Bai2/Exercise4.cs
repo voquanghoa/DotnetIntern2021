bd80e1b2f38632b7e6c72fe07e21ea5f40d9f059
luuhieu8720 <lethiluuhieu@gmail.com>
2021-06-23 11:57:41 +0700

    Add linq part2 ex04

---
+++ DotnetIntern2021/Linq/Bai2/Exercise4.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetIntern2021.Linq.Bai2
+{
+    public class Exercise4
+    {
+        public List<int> OrderByLastDigit(List<int>data)
+        {
+            return data.OrderBy(data => Math.Abs(data % 10))
+                       .Select(data => data)
+                       .ToList();
+        }
+    }
+}
