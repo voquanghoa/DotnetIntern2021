315b3ce839d8c788c7398176cad0517f8972bde8
luuhieu8720 <lethiluuhieu@gmail.com>
2021-06-23 12:00:48 +0700

    Add linq part2 ex07

---
+++ DotnetIntern2021Test/Linq/Bai2/TestExercise7.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotnetIntern2021.Linq.Bai2;
+using NUnit.Framework;
+namespace DotnetIntern2021Test.Linq.Bai2
+{
+    class TestExercise7
+    {
+        private readonly Exercise7 exercise7 = new();
+        [Test]
+        public void TestEx7()
+        {
+            var data = new List<int>
+            {
+                11, 11, 11, 20, 51, 46, 20, 11, -63, 20, 51, 20, -28, 20
+            };
+            var answer = new List<int>
+            {
+                11, 20, 51
+            };
+            Assert.AreEqual(exercise7.RepeatedNumbers(data), answer);
+        }
+    }
+}
