c580e82b09abda2c0a3f20c1688bafae16f82c9e
luuhieu8720 <lethiluuhieu@gmail.com>
2021-06-23 09:58:47 +0700

    Add linq part2 ex01

---
+++ DotnetIntern2021/Linq/Bai2/Exercise1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetIntern2021.Linq.Bai2
+{
+    public class Exercise1
+    {
+        private List<int> data = new List<int>
+        {
+            1, 2, 3, 4, 5, 6, 7, -7, -6, -5, -4, -3, -2, -1
+        };
+        public List<int> ThreeBiggestNumbers()
+        {
+            return data.OrderByDescending(data => data)
+                       .Take(3)
+                       .ToList();
+        }
+    }
+}
---
+++ DotnetIntern2021Test/Linq/Bai2/TestExercise1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotnetIntern2021.Linq.Bai2;
+using NUnit.Framework;
+
+namespace DotnetIntern2021Test.Linq.Bai2
+{
+    class TestExercise1
+    {
+        private readonly Exercise1 exercise1 = new();
+        [Test]
+        public void TestEx01()
+        {
+            var answer = new List<int>()
+            {
+                7, 6, 5
+            };
+            Assert.AreEqual(exercise1.ThreeBiggestNumbers(), answer);
+        }
+    }
+}
