632407229f1785f229dbfc077cfcf6e165fcfe0c
luuhieu8720 <lethiluuhieu@gmail.com>
2021-06-23 11:58:48 +0700

    Add linq part2 ex05

---
+++ DotnetIntern2021/Linq/Bai2/Exercise5.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetIntern2021.Linq.Bai2
+{
+    public class Exercise5
+    {
+        public int MostRepeatedNumber(List<int>data)
+        {
+            return data.GroupBy(i => i)
+                       .OrderByDescending(g => g.Count())
+                       .Select(g => g.Key)
+                       .First();
+        }
+    }
+}
