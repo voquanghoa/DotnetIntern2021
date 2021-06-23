c90b7c2f692b14beafc43704b548db4ebf6b151e
luuhieu8720 <lethiluuhieu@gmail.com>
2021-06-23 11:56:17 +0700

    Add linq part2 ex03

---
+++ DotnetIntern2021/Linq/Bai2/Exercise3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetIntern2021.Linq.Bai2
+{
+    public class Exercise3
+    {
+        private List<int> data = new List<int>
+        {
+            1, 2, 3, 4, 5, 6, 7, -7, -6, -5, -4, -3, -2, -1
+        };
+        public int RandomNumbers(int n)
+        {
+            var ran = new Random();
+            return data.OrderBy(data => ran.Next())
+                       .Take(n)
+                       .ToList()
+                       .Count;
+        }
+    }
+}
---
+++ DotnetIntern2021Test/Linq/Bai2/TestExercise3.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotnetIntern2021.Linq.Bai2;
+using NUnit.Framework;
+
+namespace DotnetIntern2021Test.Linq.Bai2
+{
+    class TestExercise3
+    {
+        private readonly Exercise3 exercise3 = new();
+        [TestCase(5)]
+        [TestCase(8)]
+        public void TestEx03(int n)
+        {
+            Assert.AreEqual(exercise3.RandomNumbers(n), n);
+        }
+    }
+}
