024e10a0eecbcd86fbb4b72acdf0bb1c9cf2aa52
luuhieu8720 <lethiluuhieu@gmail.com>
2021-06-23 11:59:49 +0700

    Add linq part2 ex06

---
+++ DotnetIntern2021/Linq/Bai2/Exercise6.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetIntern2021.Linq.Bai2
+{
+    public class Exercise6
+    {
+        public List<int> NoneRepeatedNumber(List<int> data)
+        {
+            return data.GroupBy(i => i)
+                       .Where(g => g.Count() == 1)
+                       .Select(g => g.Key)
+                       .ToList();
+        }
+    }
+}
---
+++ DotnetIntern2021Test/Linq/Bai2/TestExercise6.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotnetIntern2021.Linq.Bai2;
+using NUnit.Framework;
+namespace DotnetIntern2021Test.Linq.Bai2
+{
+    class TestExercise6
+    {
+        private readonly Exercise6 exercise6 = new();
+        [Test]
+        public void TestEx6()
+        {
+            var data = new List<int>
+            {
+                11, 11, 11, 20, 51, 46, 20, 11, -63, 20, -47, 20, -28, 20
+            };
+            var answer = new List<int>
+            {
+                51, 46, -63, -47, -28
+            };
+            Assert.AreEqual(exercise6.NoneRepeatedNumber(data), answer);
+        }
+    }
+}
