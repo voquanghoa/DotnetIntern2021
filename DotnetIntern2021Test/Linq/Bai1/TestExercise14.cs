c8a6de61166b681d58fa03a8b606b46aab8b7159
luuhieu8720 <lethiluuhieu@gmail.com>
2021-06-23 09:36:02 +0700

    Add linq ex14

---
+++ DotnetIntern2021Test/Linq/Bai1/TestExercise14.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using DotnetIntern2021.Linq.Bai1;
+
+namespace DotnetIntern2021Test.Linq.Bai1
+{
+    class TestExercise14
+    {
+        private readonly Exercise14 exercise14 = new();
+        [Test]
+        public void TestEx14()
+        {
+            var className = "Class A";
+            var answer = new List<string>
+            {
+                "Xin Chao", "Bo Gum", "Min Ho", 
+                "John Leo", "OK OK", "Back Pink"
+            };
+        }
+    }
+}
